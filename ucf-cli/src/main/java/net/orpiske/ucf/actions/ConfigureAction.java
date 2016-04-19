273cf43c867294a8112528aff5f1ff2b1920c0b4
Otavio R. Piske <angusyoung@gmail.com>
2016-04-19 17:59:31 +0200

    WIP: added dependencies, improved error logging

--- ucf-cli/src/main/java/net/orpiske/ucf/actions/ConfigureAction.java
+++ ucf-cli/src/main/java/net/orpiske/ucf/actions/ConfigureAction.java
@@ -1,6 +1,8 @@
 package net.orpiske.ucf.actions;
 
 import net.orpiske.ucf.driver.Driver;
+import net.orpiske.ucf.engine.ConfigurationEngine;
+import net.orpiske.ucf.engine.DefaultEngine;
 import net.orpiske.ucf.provider.Provider;
 import net.orpiske.ucf.render.ConfigurationRender;
 import org.apache.commons.cli.*;
@@ -19,6 +21,7 @@ public class ConfigureAction extends Action {
     private ConfigurationRender render;
     private Driver driver;
     private Provider provider;
+    private ConfigurationEngine engine;
 
     public ConfigureAction(String[] args) throws Exception {
         processCommand(args);
@@ -32,7 +35,7 @@ public class ConfigureAction extends Action {
             System.err.println("Driver for " + driverName + " not found in the classpath");
             throw e;
         } catch (IllegalAccessException e) {
-            System.err.println("Invalid driver " + driverName);
+            System.err.println("Illegal driver " + driverName);
             throw e;
         } catch (ClassNotFoundException e) {
             System.err.println("Invalid driver " + driverName);
@@ -47,7 +50,7 @@ public class ConfigureAction extends Action {
             System.err.println("Driver for " + renderName + " not found in the classpath");
             throw e;
         } catch (IllegalAccessException e) {
-            System.err.println("Invalid driver " + renderName);
+            System.err.println("Illegal driver " + renderName);
             throw e;
         } catch (ClassNotFoundException e) {
             System.err.println("Invalid driver " + renderName);
@@ -62,7 +65,7 @@ public class ConfigureAction extends Action {
             System.err.println("Provider for " + providerName + " not found in the classpath");
             throw e;
         } catch (IllegalAccessException e) {
-            System.err.println("Invalid provider " + providerName);
+            System.err.println("Illegal provider " + providerName);
             throw e;
         } catch (ClassNotFoundException e) {
             System.err.println("Invalid provider " + providerName);
@@ -71,6 +74,7 @@ public class ConfigureAction extends Action {
     }
 
     protected void processCommand(String[] args) throws Exception {
+        System.out.println("Processing arguments");
         if (args.length == 0) {
             System.err.println("Not enough parameters");
 
@@ -83,12 +87,14 @@ public class ConfigureAction extends Action {
         }
 
         render = createRenderByName("Jinja2", "net.orpiske.ucf.render.Jinja2Render");
-        // provider = createProviderByName()
+        provider = createProviderByName("Default", "net.orpiske.ucf.provider.FileSystemProvider");
+
+        engine = new DefaultEngine(driver, render, provider);
     }
 
     public int run() {
 
-
+        engine.configure();
 
         return 0;
     }
