75520e15b0206911bc5f0d40ca69eb538985e5af
Otavio R. Piske <angusyoung@gmail.com>
2016-04-20 22:06:26 +0200

    FIX: documentation fixes

--- ucf-contrib/src/main/java/net/orpiske/ucf/contrib/version/slot/SlotComparatorFactory.java
+++ ucf-contrib/src/main/java/net/orpiske/ucf/contrib/version/slot/SlotComparatorFactory.java
@@ -22,31 +22,31 @@ package net.orpiske.ucf.contrib.version.slot;
  */
 public final class SlotComparatorFactory {
 	/**
-	 * This is the default net.orpiske.ucf.contrib.version.slot: it declares a single net.orpiske.ucf.contrib.version.slot for all the versions of a
+	 * This is the default slot: it declares a single slot for all the versions of a
 	 * package
 	 */
 	public static final String DEFAULT_SLOT = "*";
 	
 	/**
-	 * The major net.orpiske.ucf.contrib.version.slot considers that versions in different major releases are different.
-	 * For instance, in this net.orpiske.ucf.contrib.version.slot definition 1.2.0 is different than 2.3.0. On the other
-	 * hand, 1.2.0 and 1.3.0 are on the same net.orpiske.ucf.contrib.version.slot.
+	 * The major slot considers that versions in different major releases are different.
+	 * For instance, in this slot definition 1.2.0 is different than 2.3.0. On the other
+	 * hand, 1.2.0 and 1.3.0 are on the same slot.
 	 */
 	public static final String MAJOR = "n.*";
 	
 	
 	/**
-	 * The major/minor net.orpiske.ucf.contrib.version.slot considers that versions in different major and minor releases
+	 * The major/minor slot considers that versions in different major and minor releases
 	 * are different. 
-	 * For instance, in this net.orpiske.ucf.contrib.version.slot definition 1.2.0 is different than 1.3.0 or 2.3.0. On
-	 * the other hand, 1.2.0 and 1.2.1 are on the same net.orpiske.ucf.contrib.version.slot.
+	 * For instance, in this version.slot definition 1.2.0 is different than 1.3.0 or 2.3.0. On
+	 * the other hand, 1.2.0 and 1.2.1 are on the same slot.
 	 */
 	public static final String MAJOR_MINOR = "n.n.*";
 	
 	/**
-	 * Constructs a new net.orpiske.ucf.contrib.version.slot comparator based on an input mask
-	 * @param mask the input mask representing the net.orpiske.ucf.contrib.version.slot
-	 * @return A net.orpiske.ucf.contrib.version.slot comparator for that mask or the default comparator if not valid
+	 * Constructs a new slot comparator based on an input mask
+	 * @param mask the input mask representing the slot
+	 * @return A slot comparator for that mask or the default comparator if not valid
 	 */
 	public static SlotComparator create(final String mask) {
 		if (mask.equals(MAJOR)) {
