df5f6a19b5940e971198004b1cbd75e9d835ce49
Otavio R. Piske <angusyoung@gmail.com>
2016-04-25 20:03:24 +0200

    NEW: need option

--- ucf-provider-fs/src/main/java/net/orpiske/ucf/provider/FileSystemProvider.java
+++ ucf-provider-fs/src/main/java/net/orpiske/ucf/provider/FileSystemProvider.java
@@ -8,8 +8,13 @@ import org.apache.commons.cli.Options;
  * Created by otavio on 4/19/16.
  */
 public class FileSystemProvider implements Provider {
+    private Options optionsRef;
+
     public void addOptions(Options options) {
+        options.addOption("l", "location", true, "the output path for the configuration");
         options.addOption("o", "output", true, "the output path for the configuration");
+
+        optionsRef = options;
     }
 
     @Override
